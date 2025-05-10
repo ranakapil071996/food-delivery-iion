077f724a2a9cfa6ee0d2a6691aa6c8e1749202fd
Dotpe <dotpe@DotpetekiMacBook-Pro.local>
2025-05-11 00:10:13 +0530

    fixing cors and end vars

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,23 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db';
-import authRoutes from './routes/authRoutes';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: process.env.ALLOWED_SERVER || "http://localhost:5173/",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+};
+app.use(cors(corsOptions));
+
 connectDB();
 app.use(express.json());
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
